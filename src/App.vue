97237687d9b16a373b137e05e8df9bb7a4c30e9a
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-07-22 13:44:00 +0200

    Added img and style at App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,30 @@ export default {
 </script>
 
 <style lang="scss">
+/* Generics */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+img {
+  height: auto;
+  max-width: 100%;
+}
+
+ul {
+  list-style-type: none;
+}
+
+a{
+  color: #000;
+  text-decoration: none;
+}
+
+/* Utils */
+.container{
+  margin: 0 auto;
+  max-width: 1400px;
+}
 </style>
