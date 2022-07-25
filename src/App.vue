e403d096bf18c767cf2e757ebbf3d93eb5d85903
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-07-25 13:53:00 +0200

    Added productsection with v-for

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
     <BaseHeader />
-    <BaseMain />
+    <main>
+      <ProductSection />
+    </main>
     <BaseFooter />
   </div>
 </template>
 
 <script>
 import BaseHeader from './components/BaseHeader.vue';
-import BaseMain from './components/BaseMain.vue';
+import ProductSection from './components/ProductSection.vue';
 import BaseFooter from './components/BaseFooter.vue'
 export default {
   name: 'DComics',
   components : {
     BaseHeader,
-    BaseMain,
+    ProductSection,
     BaseFooter
   }
 }
--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -96,7 +96,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../assets/sass/vars";
 footer{
     background-image: url(../assets/img/footer-bg.jpg);
